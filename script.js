7d05beff72760667083207528067f96d5ef347c0
Favourene <igbinosaiwinosa01@gmail.com>
2022-09-28 15:14:54 +0100

    new commit

---
+++ script.js
@@ -0,0 +1,122 @@
+// const
+
+const NAME = 'Efosa'
+
+// let
+
+let school = 'Unilag'
+
+// var
+var house = 'duplex'
+
+// Data Types
+
+// number
+let phoneNumber = 08066067
+
+// string
+
+let sentence = `Her mother's daughter`
+
+//boolean
+
+let avail = false
+
+// undefined
+
+let boss = undefined
+
+// null
+
+let pack = null
+
+// object
+
+const SOUP = {
+  meat: 40,
+  oil: 'Vegetable oil',
+  pepper: true,
+  spices: {
+    pepper: '10kg',
+    salt: {
+      mrChef: '2g',
+    },
+  },
+}
+const EFOSA = {
+  name: 'Ugly girl',
+  age: '5 years',
+  height: `4'11`,
+}
+
+let amount = 250 - 70
+
+//180
+//conditionals
+// if (amount > 200) {
+//   alert('This value is huge')
+// } else if (amount === 100) {
+//   alert('This is 100')
+// } else if (amount >= 300) {
+//   alert('Value is in 300')
+// } else {
+//   alert('not here')
+// }
+
+//? ternary operator
+// amount > 200 ? alert('hey') : alert('nay')
+
+// logical operators
+
+// AND && == ===
+
+// if (amount === 180) {
+//   alert('trues')
+// } else {
+//   alert('false')
+// }
+
+// OR ||
+
+//NOT !
+
+// alert(amount)
+
+//Arrays
+
+// []
+
+let animalname = ['lion', 'goat', 'sheep', 'dog']
+
+
+const FOOD = [
+  {
+    amala: 20,
+  },
+  {
+    amala: 40,
+  },
+  {
+    amala: 60,
+  },
+]
+
+// alert(FOOD[2].amala)
+
+// push adds a new value to array 
+
+// const NEWANIMAL = animalname.shift()
+// console.log(animalname.push())
+
+
+// pop this removes an element from the end 
+
+
+// shift removes an element from the beginning 
+
+
+
+
+
+
+
